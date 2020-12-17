a55b727744aa582d4d1d0cf3b28f8b4569b89a0e
YuriDr <yurdok77@gmail.com>
2020-12-17 23:17:18 +0200

    тестирую гид.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,6 +12,7 @@ class Header extends Component {
             <div className= 'header'>
                 <div className='headerPlus' onClick={showCreateItem}>+</div>
                <div>ToDoShechka</div> 
+               <div>привет</div>
             </div>
         );
     }
